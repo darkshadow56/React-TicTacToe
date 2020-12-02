cf9204c1c14935f2e6ee505d903de20bb1768aaa
darkshadow56 <vshinde1998@gmail.com>
2020-12-02 23:35:16 +0530

    This is tictactoe game using react hooks

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React, {useState} from 'react';
+import Icon from './Components/Icon';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Card, Container, Row, Col, Button, CardBody} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+// Declaring array
+const itemArray = new Array(9).fill("empty")
+//array ends
+//app function starts
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+  
+
+  //reload function starts
+const reloadGame = () => {
+  setIsCross(false);
+  setWinMessage("");
+  itemArray.fill("empty", 0, 9);
+  
+}
+//function to check winner starts here
+const checkIsWinner =() => {
+if (itemArray[0] === itemArray[1] &&
+    itemArray[0] === itemArray[2] &&
+    itemArray[0] !== "empty") 
+ {
+  setWinMessage(`${itemArray[0]} wins`)
+ }
+else if(itemArray[3] !== "empty" &&
+itemArray[3] === itemArray[4] &&
+itemArray[4] === itemArray[5])
+{
+  setWinMessage(`${itemArray[3]} wins`)
+} 
+else if(itemArray[6] !== "empty" &&
+itemArray[6] === itemArray[7] &&
+itemArray[7] === itemArray[8])
+{
+  setWinMessage(`${itemArray[6]} wins`)
+} 
+else if(itemArray[0] !== "empty" &&
+itemArray[0] === itemArray[3] &&
+itemArray[3] === itemArray[6])
+{
+  setWinMessage(`${itemArray[0]} wins`)
+} 
+else if(itemArray[1] !== "empty" &&
+itemArray[1] === itemArray[4] &&
+itemArray[4] === itemArray[7])
+{
+  setWinMessage(`${itemArray[1]} wins`)
+} 
+else if(itemArray[2] !== "empty" &&
+itemArray[2] === itemArray[5] &&
+itemArray[5] === itemArray[8])
+{
+  setWinMessage(`${itemArray[2]} wins`)
+} 
+else if(itemArray[0] !== "empty" &&
+itemArray[0] === itemArray[4] &&
+itemArray[4] === itemArray[8])
+{
+  setWinMessage(`${itemArray[0]} wins`)
+} 
+else if(itemArray[2] !== "empty" &&
+itemArray[2] === itemArray[4] &&
+itemArray[4] === itemArray[6])
+{
+  setWinMessage(`${itemArray[2]} wins`)
+}
+};
+
+//CheckWin method ends here
+//function to change/switch player sttarts here 
+const changeItem = itemNumber => {
+  if (winMessage){
+    return toast(winMessage, { type: "success" });
+  }
+  if(itemArray[itemNumber] === "empty"){
+itemArray[itemNumber] = isCross ? "cross" : "circle";
+setIsCross(!isCross);
+  }else{
+    return toast("already filled", {type: "error"});
+  }
+checkIsWinner();
+};
+  return (
+    
+    <Container className="p-4" >
+    <ToastContainer position="bottom-center"/>
+    <Row>
+      <Col md={6} className="offset-md-3">
+        {winMessage ? (<div className="mb-2 mt-2"><h1 className="text-success text-uppercase text-center">
+          {winMessage}</h1>
+          <Button className="reload-button" color="success" block onClick={reloadGame} >Reload The Game</Button></div>) : (
+            <h1 className="text-center turn-text pb-4">{isCross ? "Cross" : "Circle"}s turn</h1>
+          ) }
+        <div className="grid">
+          {itemArray.map((item, index) => (
+            <Card color="warning" onClick={ () => changeItem(index) }>
+            <CardBody className="box">
+              <Icon name={item} />
+            </CardBody>
+            </Card>
+          ))}
+        </div>
+      </Col>
+    </Row>
+    </Container> 
+  );
+};
+
+
+export default App;
